c34ad88db11fc48aa9ba92b9bcb02579556b8995
Dominik T. <hello@dtampe.com>
2025-09-30 02:46:27 +0200

    feat: remove outdated course posts and enhance navigation between lessons

--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -4,9 +4,13 @@ import { getCollection } from 'astro:content'
 export function sortPostsByDate(itemA: Course, itemB: Course) {
   return new Date(itemB.data.date).getTime() - new Date(itemA.data.date).getTime()
 }
+export function sortByFileName(itemA: Course, itemB: Course) {
+  return itemA.id.localeCompare(itemB.id)
+}
 
 export async function getCourses(path?: string, collection: 'course' = 'course') {
   return (await getCollection(collection, (post) => {
     return (import.meta.env.PROD ? post.data.draft !== true : true) && (path ? post.id.includes(path) : true)
-  })).sort(sortPostsByDate)
+  }))
+    .sort(sortByFileName)
 }
